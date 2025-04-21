ec9fc99a87fa8cbdd11af3919f899d3c1a6fa9d7
Christoph Ilse <ilse.christoph@gmail.com>
2025-04-21 15:33:22 +0200

    Added example images an started to add minimal kitti example for quickstart

--- EdgeYoloDepth/train_model.sh
+++ EdgeYoloDepth/train_model.sh
@@ -6,9 +6,9 @@ USER_NAME=$(whoami)
 export UID=$(id -u)
 export GID=$(id -g)
 # make sure the mappings to /training is correct and that /training contains the "train_params.yaml" config file expected by Dockerfile.Train!
-# -v /PATH/TO/MOUNT:/mnt/shared_dir -> genreal mount -> make sure to adjust paths in the config file according to this mount point!
+# -v /PATH/TO/MOUNT:/mnt/EdgeDepth -> genreal mount -> make sure it contains the train data, EdgeYoloDepth Repo etc. If necessary adjust paths in the config file according to this mount 
 docker run -u $UID:$GID -d \
     -v /PATH/TO/TRAIN/CONFIG:/training \
-    -v /PATH/TO/MOUNT:/mnt/shared_dir \
+    -v /PATH/TO/EdgeDepth:/mnt/EdgeDepth \
     -- rm --shm-size 16G --name edgeyolo_train_01_$USER_NAME edgeyolo_train_$USER_NAME
 
